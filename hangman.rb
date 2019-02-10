f08a81e551ce6958e820b8892fb149ea194f8995
Kyle Stewart <kyle4@vt.edu>
2019-02-10 15:23:46 -0500

    Added game-save feature using JSON serialization.

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,38 @@
-class Hangman
-  attr_reader :current_progress
+require "json"
 
-  def initialize(dictionary_file)
-    @dictionary = load_dictionary(dictionary_file)
-    @secret_word = random_word.split("")
-    @guesses_remaining = 6
-    @current_progress = ["*"]*@secret_word.length
-    @guess_history = []
+module Serialize
+  @@serializer = JSON
+
+  def serialize
+    obj={}
+    instance_variables.each do |var|
+      obj[var] = instance_variable_get(var)
+    end
+
+    @@serializer.dump obj
+  end
+
+  def unserialize(string)
+    obj = @@serializer.parse(string)
+    obj.keys.each do |key|
+      instance_variable_set(key, obj[key])
+    end
+  end
+end
+
+class Hangman
+  include Serialize
+
+  attr_reader :current_progress, :guesses_remaining
+
+  def initialize
+    puts "Welcome to hangman!"
+    puts "Would you like to start a new game or load a saved one?"
+    puts "1. New Game"
+    puts "2. Load Game"
+
+    new_or_load
+    game_loop
   end
 
   def secret_word
@@ -82,16 +108,57 @@ class Hangman
       puts
       if @current_progress==@secret_word
         puts "You win!"
-        break
+        return
+      end
+      puts "Enter 1 to save and quit. Press any other key to continue."
+      begin
+        choice = gets.chomp.to_i
+      rescue
+        puts "Invalid character entered"
+        retry
+      end
+      if choice==1 and @guesses_remaining>0
+        save_game
+        puts "See you later!"
+        return
       end
     end
+    puts "You lose!"
   end
 
   def save_game
-
+    file_name = "savefile"
+    save_data = serialize
+    File.open(file_name, "w"){|file| file.puts save_data}
   end
 
+  def load_game
+    file_name = "savefile"
+    save_data = File.read(file_name)
+    self.unserialize(save_data)
+  end
+  
+  def new_or_load
+    begin
+      choice = gets.chomp.to_i
+      if choice != 1 and choice != 2
+        raise
+      end
+    rescue
+      puts "Enter 1 or 2"
+      retry
+    end
+
+    if choice==1
+      @dictionary = load_dictionary("5desk.txt")
+      @secret_word = random_word.split("")
+      @guesses_remaining = 6
+      @current_progress = ["*"]*@secret_word.length
+      @guess_history = []
+    else
+      load_game
+    end
+  end
 end
 
-game = Hangman.new("5desk.txt")
-game.game_loop
+game = Hangman.new
